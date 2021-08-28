407830eb60d39b8e5cfd3153cb4d8bc1b090735e
aarti160513 <prefer_not_to_disclose@gmail.com>
2021-08-28 13:11:22 +1000

    challenge3 solution bonus1

--- coding-challenge/challenge3.js
+++ coding-challenge/challenge3.js
@@ -1,10 +1,12 @@
 const dolphinsAvgScore = (96 + 108 + 89) / 3;
 const koalasAvgScore = (89 + 91 + 10) / 3;
 
-if (dolphinsAvgScore > koalasAvgScore) {
-    console.log("Dolphins are the winner");
-} else if (koalasAvgScore === koalasAvgScore ) {
+if (dolphinsAvgScore > koalasAvgScore && dolphinsAvgScore >= 100) {
+    console.log(`Dolphins are the winner and the winning score is ${dolphinsAvgScore}`);
+} else if (koalasAvgScore === koalasAvgScore && dolphinsAvgScore >= 100) {
     console.log("Scores are tied");
+} else if (koalasAvgScore > dolphinsAvgScore && dolphinsAvgScore >= 100) {
+    console.log(`Koalas are the winner and the winning score is ${koalasAvgScore}`);
 } else {
-    console.log("Koalas are the winner");
+    console.log(`Unfortunately There are no Winners today :(, the scores are ${dolphinsAvgScore}, ${koalasAvgScore}`);
 }
