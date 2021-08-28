c14743dc775a813da7cc72658854c806f8f3c714
aarti160513 <prefer_not_to_disclose@gmail.com>
2021-08-28 12:56:13 +1000

    challenge3 solution

---
+++ coding-challenge/challenge3.js
@@ -0,0 +1,10 @@
+const dolphinsAvgScore = (96 + 108 + 89) / 3;
+const koalasAvgScore = (89 + 91 + 10) / 3;
+
+if (dolphinsAvgScore > koalasAvgScore) {
+    console.log("Dolphins are the winner");
+} else if (koalasAvgScore === koalasAvgScore ) {
+    console.log("Scores are tied");
+} else {
+    console.log("Koalas are the winner");
+}
