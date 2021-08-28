8d8d6b5704409641a0ce43b325bde90e6c167d87
aarti160513 <prefer_not_to_disclose@gmail.com>
2021-08-28 16:45:34 +1000

    solution part2 challenge4

---
+++ coding-challenge/part2/challenge4.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totals = [];
+const calcTip = amount => amount >= 50 && amount <= 300 ? amount * .15 : amount * .20;
+const calcAverage = (array) => {
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+        sum += array[i];
+    }
+
+    return sum / array.length;
+}
+
+for (let i = 0; i < bills.length; i++) {
+
+    const tip = calcTip(bills[i]);
+    tips.push(tip);
+    totals.push(bills[i] + tip);
+    console.log(`Bill ${bills[i]}, tip ${tips[i]}, total ${totals[i]}`);
+}
+
+console.log(`The average bill amount is ${calcAverage(totals)}`);
+
