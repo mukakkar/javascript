832e029f172a5cf9e3da7b4117321bcecd8940ee
aarti160513 <prefer_not_to_disclose@gmail.com>
2021-08-28 17:04:14 +1000

    formatted the code with prettier

--- coding-challenge/part2/challenge1.js
+++ coding-challenge/part2/challenge1.js
@@ -1,15 +1,21 @@
-'use strict';
+"use strict";
 
 const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
 const checkWinner = (team1AvgScore, team2AvgScore, nameTeam1, nameTeam2) => {
-    if (team1AvgScore >= team2AvgScore * 2) {
-        console.log(`${nameTeam1} is the winner ${team1AvgScore} vs ${team2AvgScore}`);
-    } else if (team2AvgScore >= team1AvgScore * 2) {
-        console.log(`${nameTeam2} is the winner ${team2AvgScore} vs ${team1AvgScore}`);
-    } else {
-        console.log(`Unfortunately there were no winners ${team2AvgScore}, ${team1AvgScore}`);
-    }
-}
+  if (team1AvgScore >= team2AvgScore * 2) {
+    console.log(
+      `${nameTeam1} is the winner ${team1AvgScore} vs ${team2AvgScore}`
+    );
+  } else if (team2AvgScore >= team1AvgScore * 2) {
+    console.log(
+      `${nameTeam2} is the winner ${team2AvgScore} vs ${team1AvgScore}`
+    );
+  } else {
+    console.log(
+      `Unfortunately there were no winners ${team2AvgScore}, ${team1AvgScore}`
+    );
+  }
+};
 
 const dolphinsAvgScore = calcAverage(96, 108, 89);
 const koalasAvgScore = calcAverage(89, 91, 10);
--- coding-challenge/part2/challenge3.js
+++ coding-challenge/part2/challenge3.js
@@ -1,20 +1,22 @@
-'use strict';
+"use strict";
 
 class Person {
-    
-    constructor (name, height, weight) {
-        this.name = name;
-        this.height = height;
-        this.weight = weight;
-    }
+  constructor(name, height, weight) {
+    this.name = name;
+    this.height = height;
+    this.weight = weight;
+  }
 
-    calcBMI() {
-        return this.weight / this.height ** 2;
-    }
-
+  calcBMI() {
+    return this.weight / this.height ** 2;
+  }
 }
 
 const mark = new Person("Mark", 1.69, 78);
 const john = new Person("John", 1.95, 92);
 
-console.log(`BMI information Mark ${mark.calcBMI()}, John ${john.calcBMI()}. Is Mark's BMI more ? ${mark.calcBMI() > john.calcBMI()}`);
+console.log(
+  `BMI information Mark ${mark.calcBMI()}, John ${john.calcBMI()}. Is Mark's BMI more ? ${
+    mark.calcBMI() > john.calcBMI()
+  }`
+);
